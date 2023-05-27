3dde146bcf97f0e1bc78793ef3855a2b725d1296
Aduwo Ayooluwa <aduwoayooluwa@gmail.com>
2023-05-27 05:13:05 +0100

    ft/added dictionary words

--- src/version_one/container/UserLoginInfo.tsx
+++ src/version_one/container/UserLoginInfo.tsx
@@ -19,7 +19,7 @@ const UserLoginInfo = (props: Props) => {
         
     }
     return (
-        <div className='grid place-items-center w-full h-full'>
+        <div className='grid place-items-center px-6 md:px-0 w-full h-full'>
             
             <div className='h-[200px] grid place-items-center py-6 leading-10 w-full md:w-1/3'>
             <p className={`${message === "" && "hidden"} text-red-600 mb-3 text-lg font-semibold text-center`}>{message}</p>
